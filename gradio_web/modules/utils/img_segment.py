c144ec67e8e2f8e60c58d2a7e390f6b8942f9aad
BinciLuo <binciluo@icloud.com>
2024-03-12 15:00:15 +0800

    [update] erode mask

--- gradio_web/modules/utils/img_segment.py
+++ gradio_web/modules/utils/img_segment.py
@@ -15,7 +15,6 @@ def erode_image(image: Image.Image, erode_range: int):
 
     # 使用滤波器进行腐蚀操作
     gray_image = gray_image.filter(ImageFilter.MaxFilter(3))
-    #gray_image = gray_image.filter(ImageFilter.BoxBlur(2))
     eroded_image = gray_image.filter(ImageFilter.MinFilter(erode_range))
 
     # 将原始图像与腐蚀后的图像进行比较，将相同位置的像素设置为黑色

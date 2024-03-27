61a6e15afb80ce34789f80e927fd465ddb0d407d
BinciLuo <binciluo@icloud.com>
2024-03-27 18:33:20 +0800

    [update] use llama factory

---
+++ chatglm2-6b/LLaMA-Factory/my_train.sh
@@ -0,0 +1,19 @@
+CUDA_VISIBLE_DEVICES=0 python3 src/train_bash.py \
+    --stage sft \
+    --do_train \
+    --model_name_or_path /home/user/netdisk/data/chatglm2-6b \
+    --dataset chat_dataset \
+    --template default \
+    --finetuning_type lora \
+    --lora_target all \
+    --output_dir loras \
+    --overwrite_cache \
+    --per_device_train_batch_size 4 \
+    --gradient_accumulation_steps 4 \
+    --lr_scheduler_type cosine \
+    --logging_steps 10 \
+    --save_steps 100 \
+    --learning_rate 5e-5 \
+    --num_train_epochs 3.0 \
+    --plot_loss \
+    --fp16
